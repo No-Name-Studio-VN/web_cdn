56ae8a6ba6dfdd0a92b48d82ef7268649c670f4c
Lê Ngô Đức Mạnh <myt@nnsvn.me>
2024-06-02 02:47:30 +0700

    13.2.1

--- cp/sw.js
+++ cp/sw.js
@@ -1,7 +1,6 @@
 const CACHE = "nnsvn-offline-v4";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-importScripts('https://arc.io/arc-sw-core.js');
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "offline";
