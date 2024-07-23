6c613bb85f62a66c3d5c5a948a1f96e0bde77e08
Lê Ngô Đức Mạnh <myt@nnsvn.me>
2024-07-23 21:49:22 +0700

    14.0.0 public beta-4

--- cp/static/js/pages/economy/transactions.js
+++ cp/static/js/pages/economy/transactions.js
@@ -1,4 +1,4 @@
-function formatContent(t,a,e){return`
+function formatContent(t,a,n){return`
     <div class="d-flex align-items-center">
         <span class="me-3">
             <span class="p-2 bg-primary icon icon-md fs-7 rounded-4 ti ti-building-store"></span>
@@ -10,7 +10,7 @@ function formatContent(t,a,e){return`
           </div>
         </div>
     </div>
-    `}function formatAmount(t){var a=Number(t.amount),e=a<0?"-":"+",n=a<0?"text-danger":"text-success",a=Number.isInteger(a)?a.toFixed(0):a.toString();return`<span class="${n} fw-bold fs-4">${e}$${Math.abs(a)}</span> <span class="d-none">${balance_type[t.type]}</span>`}function searchAmount(t,a,e,n){return!!a.includes(t)}function sortAmount(t,a){return t.amount-a.amount}var balance_type={deposit:"Deposit",withdraw:"Withdraw"};(()=>{$("#transactionsbox").html(`
+    `}function formatAmount(t){var a=Number(t.amount),n=a<0?"-":"+",e=a<0?"text-danger":"text-success",a=Number.isInteger(a)?a.toFixed(0):a.toString();return`<span class="${e} fw-bold fs-4">${n}$${Math.abs(a)}</span> <span class="d-none">${balance_type[t.type]}</span>`}function searchAmount(t,a,n,e){return!!a.includes(t)}function sortAmount(t,a){return t.amount-a.amount}var balance_type={deposit:"Deposit",withdraw:"Withdraw"};(()=>{$("#transactionsbox").html(`
     <div class="table-responsive">
         <div id="trans_toolbar"></div>
         <table id="trans_table" data-flat="true">
@@ -21,4 +21,4 @@ function formatContent(t,a,e){return`
                 </tr>
             </thead>
         </table>
-    </div>`);const e=$("#trans_table");function n(){ContentHandler.ToggleMultiSelect($(".bootstrap-table-filter-control-amount"),{placeholder:"Type"})}$.get("/economy/transactions/history",function(t){t=t.data;t.forEach(t=>{t.amount={amount:"withdraw"===t.type?"-"+t.amount:t.amount,type:t.type},t.content={source:t.source,timestamp:t.timestamp,currency:t.currency,location:t.location}}),e.bootstrapTable({...window.CustomBT_Config,cookieIdTable:"trans_table",toolbar:"#trans_toolbar",data:t}),n();let a=!1;e.on("toggle.bs.table",()=>{(a=!a)&&setTimeout(n,500)}),ContentHandler.toggleMainContent("#transactionsbox",!0)})})();
+    </div>`);const n=$("#trans_table");function e(){window.ContentHandler.ToggleMultiSelect($(".bootstrap-table-filter-control-amount"),{placeholder:"Type"})}$.get("/economy/transactions/history",function(t){t=t.data;t.forEach(t=>{t.amount={amount:"withdraw"===t.type?"-"+t.amount:t.amount,type:t.type},t.content={source:t.source,timestamp:t.timestamp,currency:t.currency,location:t.location}}),n.bootstrapTable({...window.CustomBT_Config,cookieIdTable:"trans_table",toolbar:"#trans_toolbar",data:t}),e();let a=!1;n.on("toggle.bs.table",()=>{(a=!a)&&setTimeout(e,500)}),window.ContentHandler.toggleMainContent("#transactionsbox",!0)})})();
--- cp/static/sw.js
+++ cp/static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "nnsvn-offline-v4";
+const CACHE = "nnsvn-offline-v5";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
